6b2b1b57081c7a17cc8b5d73a8aa236e5ad34491
mi365-dotcom <mi365@exeter.ac.uk>
2026-02-13 14:16:41 +0000

    california

--- CopyArrayApp.java
+++ CopyArrayApp.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
+
+
 public class CopyArrayApp{
   public static void main(String[] args) {
     int nVals = args.length;
+    // to create a new array of ints
+    int[] myVals = new int[nVals]; 
     for (int i = 0; i < nVals; i++) {
       int item = Integer.parseInt(args[i]);
-      System.out.println(item);
+      // to allocate a value into the created array at index i
+      myVals[i] = item;
     }
+    System.out.println("Original values: " + Arrays.toString(myVals));
   }
 }
