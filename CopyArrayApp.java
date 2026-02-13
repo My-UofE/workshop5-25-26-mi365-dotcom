af67807037a81c009d5004bb86c297a3eb00c9e3
mi365-dotcom <mi365@exeter.ac.uk>
2026-02-13 14:25:44 +0000

    Point 1 done

--- CopyArrayApp.java
+++ CopyArrayApp.java
@@ -1,16 +1,32 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 
 public class CopyArrayApp{
   public static void main(String[] args) {
+
     int nVals = args.length;
+    
     // to create a new array of ints
     int[] myVals = new int[nVals]; 
+    
     for (int i = 0; i < nVals; i++) {
+      
       int item = Integer.parseInt(args[i]);
+      
       // to allocate a value into the created array at index i
       myVals[i] = item;
     }
+
     System.out.println("Original values: " + Arrays.toString(myVals));
+
+    ArrayList<Integer> uniqueVals = new ArrayList<>();
+
+    for(int i = 0; i < myVals.length; i++ ){
+
+        int values = myVals[i];
+    }
+
+    System.out.println("Unique values: " + uniqueVals);
   }
 }
